4cb7a9530b0befa11bad656688bae74f41db8af7
Jonathan <jonathan.grangien@gmail.com>
2014-11-26 22:08:40 +0100

    Remove textview and settings button to show them as actionbar instead

--- src/com/example/mapus/Start.java
+++ src/com/example/mapus/Start.java
@@ -14,19 +14,21 @@ public class Start extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start);
-		getActionBar().hide();
+		getActionBar().setDisplayShowHomeEnabled(false);
 		
-		ImageView settingsbtn = (ImageView) findViewById(R.id.settingsbutton);
+		//ImageView settingsbtn = (ImageView) findViewById(R.id.settingsbutton);
 		ImageView mapsbtn = (ImageView) findViewById(R.id.mapsbutton);
 		ImageView contactsbtn = (ImageView) findViewById(R.id.contactsbutton);
 		ImageView checkoutbtn = (ImageView) findViewById(R.id.checkoutbutton);
 		
+		/*
 		settingsbtn.setOnClickListener(new View.OnClickListener() {	
 		@Override
 		public void onClick(View v) {
 			startActivity(new Intent("com.example.mapus.MENUONE"));	
 			}
 		});
+		*/
 		
 		mapsbtn.setOnClickListener(new View.OnClickListener() {	
 			@Override
