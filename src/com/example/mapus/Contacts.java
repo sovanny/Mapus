6ca4eaef86e7164fd0967d75ddc3ec1003d71d39
Jonathan <jonathan.grangien@gmail.com>
2014-11-27 21:29:17 +0100

    Add action bar functionality to more activities
    Made start parent to contacts and settings
    Added activity titles to contacts and settings

--- src/com/example/mapus/Contacts.java
+++ src/com/example/mapus/Contacts.java
@@ -3,6 +3,11 @@ package com.example.mapus;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +16,9 @@ public class Contacts extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contacts);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);		//action bar back button
+        getActionBar().setDisplayShowHomeEnabled(false);	//hide app icon in action bar
 		
 		Button settingsbtn = (Button) findViewById(R.id.settingsbutton);
 		Button addbtn = (Button) findViewById(R.id.addcontactbutton);
@@ -29,4 +37,30 @@ public class Contacts extends Activity {
 				}
 			});
 	}
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu items for use in the action bar
+      MenuInflater inflater = getMenuInflater();
+   
+      inflater.inflate(R.menu.second, menu);
+      return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+          NavUtils.navigateUpFromSameTask(this);
+          return true;
+        case R.id.action_direct_settings:		//SETTINGS-KNAPPEN
+        	Log.d("MapLog", "Hejhej");
+        	startActivity(new Intent("com.example.mapus.SETTINGS"));
+        	break;
+        	
+      }
+   
+      return super.onOptionsItemSelected(item);
+    }
 }
