6fd7eef13dc23600457145ea8fe0ae4ab9f32f02
Jonathan <jonathan.grangien@gmail.com>
2014-12-03 01:27:35 +0100

    Add marker listener -- logs coordinates
    coordinates are varying, not fixed. Can it be helped? Consequences?

--- src/com/example/mapus/MapActivity2.java
+++ src/com/example/mapus/MapActivity2.java
@@ -1,10 +1,13 @@
 package com.example.mapus;
 
 import com.qozix.tileview.TileView;
+import com.qozix.tileview.markers.MarkerEventListener;
 //import com.qozix.tileview.markers.MarkerEventListener;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MapActivity2 extends Activity{
@@ -25,25 +28,39 @@ public class MapActivity2 extends Activity{
         tileView.addDetailLevel(0.25f, "tiles/250_%col%_%row%.png", "samples/tp5.png");
         tileView.addDetailLevel(0.125f, "tiles/125_%col%_%row%.png", "samples/tp5.png");
 
-     // Add the view to display it
-        setContentView(tileView);
-        
         ImageView markerA = new ImageView(this);
         markerA.setImageResource(R.drawable.map_marker_blue);
-        markerA.setTag("Nice");
+        markerA.setTag("Test1");
 
         ImageView markerB = new ImageView(this);
         markerB.setImageResource(R.drawable.map_marker_blue);
-        markerB.setTag("Paris");
+        markerB.setTag("Test2");
 
         tileView.addMarker(markerA, 300, 300);
         tileView.addMarker(markerB, 300, 500);
         
-        //activate cache?
-//        tileView.setCacheEnabled(true);
+        tileView.addMarkerEventListener(new MarkerEventListener(){
+        	  @Override
+        	  public void onMarkerTap( View view, int x, int y ){
+        	    Log.d("Marker Event", "marker tag = " + view.getTag() + ", coordinates (X:Y) = " + x + ":" + y );
+        	  }
+        	});
+        
+        // use pixel coordinates to roughly center it
+        // they are calculated against the "full" size of the mapView 
+        // i.e., the largest zoom level as it would be rendered at a scale of 1.0f
+        // tileView.moveToAndCenter(2550,1970);
+        // tileView.slideToAndCenter(2550,1970);
+
+        // Set the default zoom (zoom out by 4 => 1/4 = 0.25)
+        // tileView.setScale( 0.25 );
         
         
+        //You can activate the cache if you are using remote pictures with the following code
+        //tileView.setCacheEnabled(true);
+        
+        // Add the view to display it
+        setContentView(tileView);
     }
-    
     
 }
