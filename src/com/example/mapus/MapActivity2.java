0493f68855d09c4b5fbcec3e8bb1655429c818bb
Jonathan <jonathan.grangien@gmail.com>
2014-12-15 22:39:13 +0100

    Small implementation of variable re-referencing

--- src/com/example/mapus/MapActivity2.java
+++ src/com/example/mapus/MapActivity2.java
@@ -66,7 +66,9 @@ public class MapActivity2 extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActionBar().setDisplayShowHomeEnabled(false);
-
+        
+        //reference the TileView multiple times
+     	TileView tileView = getTileView();
 
         mContext = this;
 
